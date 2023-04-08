3b23361075665e79add552fbc5928c97025c3593
codetrybe <tpauldike@yahoo.com>
2023-04-08 05:57:41 +0000

    assignment

--- DIR-C/for-loop-and-_putchar/_putchar.c
+++ DIR-C/for-loop-and-_putchar/_putchar.c
@@ -1,9 +1,16 @@
+/* emulate the C standard library function, putchar() */
 #include <unistd.h>
+#include <stdio.h>
 
 ssize_t _putchar(char *str);
 int main(void)
 {
-	_putchar("Omolola\n Omotalade");
+/* the putchar() funtion works like this */
+	putchar('a');
+	putchar('\n');
+/* your own _putchar() fuction should do the same */
+	_putchar('a');
+	_putchar('\n');
 
 	return 0;
 }
