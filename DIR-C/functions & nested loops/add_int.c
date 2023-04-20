f4958edcbd529d5f7bc556e6a8f206eb915dd3e0
Omololaomotalade <omololaomotalade@yahoo.com>
2023-04-20 14:49:46 +0000

    if_else

--- DIR-C/functions & nested loops/add_int.c
+++ DIR-C/functions & nested loops/add_int.c
@@ -1,3 +1,14 @@
 #include "header.h"
 
+int add_int(int a, int b)
+{
+	int result;
+
+	result = a+b;
+
+	printf("This is the addition of two integers %d\n", result);
+
+	return 0;
+}
+
 
--- DIR-C/functions & nested loops/check_cohort.c
+++ DIR-C/functions & nested loops/check_cohort.c
@@ -1,3 +1,29 @@
 #include "header.h"
 
+void check_cohort(void)
+{
+	char name;
+	int cohort;
+
+	printf("What is your name");
+
+	scanf("%c", name);
+
+	printf("What cohort are you", cohort);
+
+	scanf("%d", cohort);
+
+	if (cohort != 11)
+	{
+		printf("Do not have access to this cohort");
+	}
+
+	else
+	{
+		printf("Welcome to cohort %d %c",cohort,name);
+	}
+	
+
+}
+
 
