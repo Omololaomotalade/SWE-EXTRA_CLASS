0069ae04dc8e66cbfa8fc57c464906a4061bf7bc
tpauldike <topman4loveworld@gmail.com>
2023-06-07 16:33:23 +0100

    assignment

---
+++ DIR-C/functions & nested loops/assignment/assignment.c
@@ -0,0 +1,12 @@
+/** Show the return value of add3()
+ */
+#include "main.h"
+
+int main(void)
+{
+	/*
+	 * your code goes here
+	 */
+	
+	return (0);
+}
---
+++ DIR-C/functions & nested loops/assignment/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *add3(int a, int b, int c);
+
+#endif
