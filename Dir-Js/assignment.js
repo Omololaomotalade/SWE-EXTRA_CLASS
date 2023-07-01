2c7e59ead961959280afb3a5199b7e3398f70978
tpauldike <topman4loveworld@gmail.com>
2023-07-01 07:11:24 +0100

    added more assignments

--- Dir-Js/assignment.js
+++
@@ -1,4 +0,0 @@
-// Declare a variable that houses each of the names and ages of the siblings in one index
-
-/*For exmaple, if I do console.log(infoOfSiblings[1]), it should print the name and age of the
-first sibling*/
