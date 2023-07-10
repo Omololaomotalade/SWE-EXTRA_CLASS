398e7decbb4405c39b94f43cbc2b5cea0419206f
tpauldike <topman4loveworld@gmail.com>
2023-07-10 21:12:13 +0100

    learned callback function

---
+++ Dir-Js/function/arrow_function.js
@@ -0,0 +1,21 @@
+/*
+//normal function
+function addNumber(a, b) {
+  const sum = a + b;
+  return sum;
+}
+
+rValue = addNumber(4, 5);
+console.log(rValue);
+*/
+
+//callback function
+(function addNumber(a, b) {
+  const sum = a + b;
+    console.log(sum);
+})(6, 3);
+
+//anonymous function
+
+
+//arrow function
