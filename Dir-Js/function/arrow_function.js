8ee96c1942056a17b18b69e212fa62a0507d0cf6
tpauldike <topman4loveworld@gmail.com>
2023-07-12 19:02:31 +0000

    learning arrow functions

--- Dir-Js/function/arrow_function.js
+++ Dir-Js/function/arrow_function.js
@@ -9,13 +9,23 @@ rValue = addNumber(4, 5);
 console.log(rValue);
 */
 
+/*
 //callback function
 (function addNumber(a, b) {
   const sum = a + b;
-    console.log(sum);
-})(6, 3);
+  console.log(sum);
+})(6, 4);
+*/
 
-//anonymous function
+/*
+//anonymous function: callback is necessary here
+(function (x, y) {
+  const sum = x + y;
+  console.log(sum);
+})(10, 5);
+*/
 
+//arrow function: anonymous as well and must either be called back,  or saved in a variable, or used
+//as a function's argument or used as a function's return value
+console.log(((x, y) => x + y)(12, 8));
 
-//arrow function
