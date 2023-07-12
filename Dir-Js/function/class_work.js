005464c2bd7aa1c7619ca1bc65cf6bb474e0211e
tpauldike <topman4loveworld@gmail.com>
2023-07-12 20:34:25 +0100

    modified the classwork

--- Dir-Js/function/class_work.js
+++ Dir-Js/function/class_work.js
@@ -7,5 +7,7 @@ rValue = addNumber(4, 5);
 console.log(rValue);
 
 /*
-  Write an arrow function that counts from one to any number passed to it as its argument
+  Write an arrow function that counts from 1 to any number passed to it as its argument
+
+  e.g; countOneTo(10);
  */
