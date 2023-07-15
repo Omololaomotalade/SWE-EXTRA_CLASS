4134b40dd1a9948069f421f76b89faaa33aee50d
Omololaomotalade <omololaomotalade@yahoo.com>
2023-07-15 09:24:46 +0100

    class work

--- Dir-Js/function/class_work.js
+++ Dir-Js/function/class_work.js
@@ -3,11 +3,24 @@ function addNumber(a, b) {
   return sum;
 }
 
-rValue = addNumber(4, 5);
-console.log(rValue);
+// rValue = addNumber(4, 5);
+// console.log(rValue);
 
 /*
   Write an arrow function that counts from 1 to any number passed to it as its argument
 
   e.g; countOneTo(10);
  */
+const countNumber = (x) => {
+  if (x >= 1) {
+    for (let count = 1; count <= x; count++) {
+      console.log(count);
+    }
+  } else {
+    console.log("This value is less than one or its not a number");
+  }
+};
+
+countNumber("b");
+
+exports.countNumber = countNumber;
---
+++ Dir-Js/function/index.js
@@ -0,0 +1,3 @@
+const class_work = require("./class_work");
+
+class_work.countNumber(5);
